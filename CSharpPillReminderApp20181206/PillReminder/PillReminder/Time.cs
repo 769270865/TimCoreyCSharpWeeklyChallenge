d109cdee53df3b5313435824da0f8e846098a7f5
769270865 <wly769270865@hotmail.com>
2018-12-10 01:53:54 -0800

    Added pill reminder app challenge

---
+++ CSharpPillReminderApp20181206/PillReminder/PillReminder/Time.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PillReminder
+{
+    [Serializable]
+    public struct Time
+    {
+        public int Hour { get; private set; }
+        public int Minute { get; private set; }
+        public int Second { get; private set; }
+
+        public Time(DateTime dateTime)
+        {
+            Hour = dateTime.Hour;
+            Minute = dateTime.Minute;
+            Second = dateTime.Minute;
+        }
+        public Time(int hour, int minute,int second)
+        {
+            if (hour > 23 || hour < 0)
+            {
+                throw new ArgumentException("Hour should be a number between 0 and 23");
+            }
+            if (minute > 59 || minute < 0 )
+            {
+                throw new ArgumentException("Minute should be a number between 0 and 59");
+            }
+            if (second > 59 || second < 0)
+            {
+                throw new ArgumentException("Second should be a number between 0 and 59");
+            }
+            Hour = hour; Minute = minute; Second = second;
+        }
+        
+
+    }
+}
